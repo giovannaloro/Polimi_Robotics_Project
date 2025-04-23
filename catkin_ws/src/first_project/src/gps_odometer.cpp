9392e5de398688c321bf6cba326dba83d0cda45c
simonadepalma <simona.depalma@mail.polimi.it>
2025-04-23 15:45:36 +0200

    Loaded

---
+++ catkin_ws/src/first_project/src/gps_odometer.cpp
@@ -0,0 +1,101 @@
+#include "ros/ros.h"∫
+#include "std_msgs/String.h"
+#include "sensor_msgs/NavSatFix.h"
+#include "nav_msgs/Odometry.h"
+#include <cmath>
+#include "tf/transform_broadcaster.h"
+#include "geometry_msgs/TransformStamped.h"
+
+
+const double a = 6378137;
+const double b = 6356752;
+
+class GPSOdometer{
+private: 
+	ros::Subscriber sub;
+	ros::Publisher pub;
+	tf::TransformBroadcaster tf_broadcaster;
+
+	double lat_r_, lon_r_, alt_r_;
+	double x_ref_, y_ref_, z_ref_;
+
+public: 
+	GPSOdometer() {
+
+	ros::NodeHandle n;
+
+	n.param("lat_r", lat_r_, 0.0);
+	n.param("lon_r", lon_r_, 0.0);
+	n.param("alt_r", alt_r_, 0.0);
+
+	 ROS_INFO("Reference GPS set: lat_r = %.6f, lon_r = %.6f, alt_r = %.2f", lat_r_, lon_r_, alt_r_);
+
+
+	sub = n.subscribe("/swiftnav/front/gps_pose", 1, &GPSOdometer::gpsCallback, this);
+        pub = n.advertise<nav_msgs::Odometry>("/gps_odom", 1);
+ 
+}
+
+void gpsCallback(const sensor_msgs::NavSatFix::ConstPtr& msg){
+	double e, n, u;
+	gpsToOdom(msg->latitude, msg->longitude, msg->altitude, e, n, u);
+
+	nav_msgs::Odometry odom_msg;
+	odom_msg.header.stamp = ros::Time::now();
+	odom_msg.header.frame_id = "odom";
+	odom_msg.child_frame_id = "gps";
+
+	odom_msg.pose.pose.position.x = e;
+	odom_msg.pose.pose.position.y = n;
+	odom_msg.pose.pose.position.z = u;
+
+	pub.publish(odom_msg);
+
+	tf::Transform transform;
+	transform.setOrigin(tf::Vector3(e, n, u));
+	tf::Quaternion q;
+	q.setRPY(0, 0, 0);
+
+	transform.setRotation(q);
+	tf_broadcaster.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "odom", "gps"));
+}
+
+
+void gpsToECEF(double lat, double lon, double alt, double& x, double& y, double& z){
+	double e2 = 1-(pow(b,2)/pow(a,2));
+	double N = a/sqrt(1-e2*sin(lat)*sin(lat));
+
+	x = (N + alt)*cos(lat)*cos(lon);
+	y = (N + alt)*cos(lat)*sin(lon);
+	z = (N*(1-e2)+alt) * sin(lat);
+}
+
+void ecefToENU(double x_ecef, double y_ecef, double z_ecef, double& x_enu, double& y_enu, double& z_enu){
+	double lat_rad = lat_r_ * M_PI/180.0;
+	double lon_rad = lon_r_ * M_PI/180.0;
+
+	double dx = x_ecef - x_ref_;
+	double dy = y_ecef - y_ref_;
+	double dz = z_ecef - z_ref_;
+
+	x_enu = -sin(lon_rad)*dx + cos(lon_rad) * dy;
+	y_enu = -sin(lat_rad) * cos(lon_rad) * dx - sin(lat_rad)*sin(lon_rad)*dy+cos(lat_rad)*dz;
+	z_enu = cos(lat_rad)*cos(lon_rad)*dx + cos(lat_rad)*sin(lon_rad)*dy+sin(lat_rad)*dz;
+}
+
+void gpsToOdom(double lat, double lon, double alt, double& e, double& n, double& u){
+	double x_ECEF, y_ECEF, z_ECEF;
+	gpsToECEF(lat*M_PI/180.0, lon*M_PI/180.0, alt, x_ECEF, y_ECEF, z_ECEF);
+	ecefToENU(x_ECEF, y_ECEF, z_ECEF, e, n, u); 
+}
+};
+int main(int argc, char **argv){
+  	
+	ros::init(argc, argv, "gps_odometer");
+	ros::NodeHandle n;
+	GPSOdometer gps_odometer;
+  	ros::spin();
+
+  return 0;
+};
+
